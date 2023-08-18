ac1b0211006675675dedc379442d8e2087c97299
Nagornyi <artnagornyi@gmail.com>
2023-08-18 17:33:28 +0300

    Setup webpack webpackDevServer

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -4,9 +4,10 @@ import {buildPlugins} from "./buildPlugins";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolver} from "./buildResolver";
 import {BuildOptions} from "./types/config";
+import {buildDevServer} from "./buildDevServer";
 
 export function buildWebpackConfig(options: BuildOptions ): webpack.Configuration {
-  const {mode, paths} = options;
+  const {mode, paths, isDev} = options;
 
   return {
     mode,
@@ -21,5 +22,7 @@ export function buildWebpackConfig(options: BuildOptions ): webpack.Configuratio
       rules: buildLoaders(),
     },
     resolve: buildResolver(),
+    devtool: isDev ? 'inline-source-map' : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
   }
 }
