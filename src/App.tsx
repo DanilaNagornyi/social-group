e5b29eab29e82aeb4c6fac2e36ea164b9247a62c
Nagornyi <artnagornyi@gmail.com>
2023-11-01 16:16:38 +0300

    Add theme and theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React, {Suspense} from 'react';
 import type {FC} from 'react';
-import './index.scss';
+import './styles/index.scss';
 import {Link, Route, Routes} from 'react-router-dom';
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {useTheme} from "./hooks/useTheme";
+
 
 const App: FC = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle theme</button>
       <Link to="/">Главная</Link>
       <Link to="/about">О нас</Link>
       <Suspense fallback={<div>Loading...</div>}>
