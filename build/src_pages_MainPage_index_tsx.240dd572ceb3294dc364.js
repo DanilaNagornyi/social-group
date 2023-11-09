5cb964059e72d40daf7dc5ae3119f47825742853
Nagornyi <artnagornyi@gmail.com>
2023-11-09 11:39:31 +0300

    Edit structure for FSD

--- build/src_pages_MainPage_index_tsx.240dd572ceb3294dc364.js
+++ build/src_pages_MainPage_index_tsx.240dd572ceb3294dc364.js
@@ -1,25 +1,30 @@
 "use strict";
-(self["webpackChunksocial_group"] = self["webpackChunksocial_group"] || []).push([["src_pages_MainPage_index_tsx"],{
+(self["webpackChunksocial_group"] = self["webpackChunksocial_group"] || []).push([["src_pages_MainPage_index_tsx"], {
 
-/***/ "./src/pages/MainPage/index.tsx":
-/*!**************************************!*\
-  !*** ./src/pages/MainPage/index.tsx ***!
-  \**************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+    /***/ "./src/pages/MainPage/index.tsx":
+    /*!**************************************!*\
+      !*** ./src/pages/MainPage/AboutPage.tsx ***!
+      \**************************************/
+    /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
-/* harmony export */ });
-/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+        __webpack_require__.r(__webpack_exports__);
+        /* harmony export */
+        __webpack_require__.d(__webpack_exports__, {
+            /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+            /* harmony export */
+        });
+        /* harmony import */
+        var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
 
-var MainPage = function () {
-    return (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "MainPage" }, void 0);
-};
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MainPage);
+        var MainPage = function () {
+            return (0, react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", {children: "MainPage"}, void 0);
+        };
+        /* harmony default export */
+        const __WEBPACK_DEFAULT_EXPORT__ = (MainPage);
 
 
-/***/ })
+        /***/
+    })
 
 }]);
 //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX01haW5QYWdlX2luZGV4X3RzeC4yNDBkZDU3MmNlYjMyOTRkYzM2NC5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7OztBQUFnRDtBQUNoRDtBQUNBLFdBQVcsc0RBQUksVUFBVSxzQkFBc0I7QUFDL0M7QUFDQSxpRUFBZSxRQUFRLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9zb2NpYWwtZ3JvdXAvLi9zcmMvcGFnZXMvTWFpblBhZ2UvaW5kZXgudHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGpzeCBhcyBfanN4IH0gZnJvbSBcInJlYWN0L2pzeC1ydW50aW1lXCI7XG52YXIgTWFpblBhZ2UgPSBmdW5jdGlvbiAoKSB7XG4gICAgcmV0dXJuIF9qc3goXCJkaXZcIiwgeyBjaGlsZHJlbjogXCJNYWluUGFnZVwiIH0sIHZvaWQgMCk7XG59O1xuZXhwb3J0IGRlZmF1bHQgTWFpblBhZ2U7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
--- src/pages/AboutPage/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import type { FC } from 'react';
-
-const AboutPage: FC = () => {
-  return <div>AboutPage</div>;
-};
-
-export default AboutPage;
