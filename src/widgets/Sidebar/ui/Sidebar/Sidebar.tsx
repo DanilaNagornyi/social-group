6afecd5b325c8fe6ce5667b361d2777ff87b9d85
Nagornyi <artnagornyi@gmail.com>
2024-03-24 16:07:10 +0300

    ts lint fix script

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import React, { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import s from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -10,7 +11,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
-
+    const { t } = useTranslation();
     const handleToggle = () => {
         setCollapsed((prev) => !prev);
     };
@@ -20,13 +21,13 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid="sidebar"
             className={classNames(s.Sidebar, { [s.collapsed]: collapsed }, [className])}
         >
-            {/* <button */}
-            {/*    data-testid="sidebar-toggle" */}
-            {/*    type="button" */}
-            {/*    onClick={handleToggle} */}
-            {/* > */}
-            {/*    toggle */}
-            {/* </button> */}
+            <button
+                data-testid="sidebar-toggle"
+                type="button"
+                onClick={handleToggle}
+            >
+                {t('Переключить')}
+            </button>
             <div className={s.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher />
