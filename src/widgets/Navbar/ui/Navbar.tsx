1b614ddf3df5de177ea6709a91e45f6409653ef1
Nagornyi <artnagornyi@gmail.com>
2024-03-29 09:25:54 +0300

    fix ts navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,22 +1,23 @@
 import type { FC } from 'react';
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
-// import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-// import { useTranslation } from 'react-i18next';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import s from './Navbar.module.scss';
 
 interface NavbarProps {
   className?: string;
 }
 
-export const Navbar: FC<NavbarProps> = ({ className }) =>
-// const { t } = useTranslation();
+export const Navbar: FC<NavbarProps> = ({ className }) => {
+    const { t } = useTranslation();
 
-    (
+    return (
         <div className={classNames(s.Navbar, {}, [className])}>
             <div className={s.navLink}>
-                {/* <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('Главная')}</AppLink> */}
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/">{t('Главная')}</AppLink>
                 {/* <AppLink theme={AppLinkTheme.SECONDARY} to="/about">{t('О сайте')}</AppLink> */}
             </div>
         </div>
     );
+};
