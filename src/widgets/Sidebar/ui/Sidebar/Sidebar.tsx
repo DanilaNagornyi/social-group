de073a96f8e120ce1781846c617d6a7eee2838b5
Nagornyi <artnagornyi@gmail.com>
2024-03-29 09:10:32 +0300

    upgrade sidebar and translate

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,11 @@ import React, { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
 import { useTranslation } from 'react-i18next';
+import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { RoutePath } from 'shared/config/routeConfig/routeConfig';
+import AboutIcon from 'shared/assets/icons/about-20-20.svg';
+import MainIcon from 'shared/assets/icons/main-20-20.svg';
 import s from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -21,16 +26,44 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid="sidebar"
             className={classNames(s.Sidebar, { [s.collapsed]: collapsed }, [className])}
         >
-            <button
+            <Button
                 data-testid="sidebar-toggle"
-                type="button"
                 onClick={handleToggle}
+                className={s.collapseBtn}
+                theme={ButtonTheme.BACKGROUND_INVERTED}
+                size={ButtonSize.L}
+                square
             >
-                {t('Переключить')}
-            </button>
+                {collapsed ? '>' : '<'}
+            </Button>
+            <div className={s.items}>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.main}
+                    className={s.item}
+                >
+                    <MainIcon className={s.icon} />
+                    <span className={s.link}>
+                        {t('Главная')}
+                    </span>
+                </AppLink>
+                <AppLink
+                    theme={AppLinkTheme.SECONDARY}
+                    to={RoutePath.about}
+                    className={s.item}
+                >
+                    <AboutIcon className={s.icon} />
+                    <span className={s.link}>
+                        {t('О сайте')}
+                    </span>
+                </AppLink>
+            </div>
             <div className={s.switchers}>
                 <ThemeSwitcher />
-                <LangSwitcher />
+                <LangSwitcher
+                    short={collapsed}
+                    className={s.lang}
+                />
             </div>
         </div>
     );
