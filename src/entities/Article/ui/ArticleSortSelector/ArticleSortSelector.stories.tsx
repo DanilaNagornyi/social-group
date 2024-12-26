76152dac9a3382d865728aab6f771658df5e8a3d
Nagornyi <artnagornyi@gmail.com>
2024-12-26 17:09:54 +0300

    feature-59: filter articles and useDebounce

---
+++ src/entities/Article/ui/ArticleSortSelector/ArticleSortSelector.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+
+import { ArticleSortSelector } from './ArticleSortSelector';
+
+export default {
+    title: 'entities/Article/ArticleSortSelector',
+    component: ArticleSortSelector,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof ArticleSortSelector>;
+
+const Template: StoryFn<typeof ArticleSortSelector> = (args) => <ArticleSortSelector {...args} />;
+
+export const Normal = Template.bind({});
+Normal.args = {};
